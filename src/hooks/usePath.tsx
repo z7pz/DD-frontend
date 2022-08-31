105e7db7b8cc8d9fadb9df2b4a03f69d5f1e4c20
z7pz <sdfygamer@gmail.com>
2022-08-31 21:41:56 +0300

    fix: bug fixes

--- src/hooks/usePath.tsx
+++ src/hooks/usePath.tsx
@@ -1,8 +1,5 @@
-import { useMatch } from "@tanstack/react-location";
-
 export function usePath(arr: string[]) {
-    // const { pathname } = useMatch();
-    const type = (window.location.pathname).split("/").filter(Boolean)[1];
-    const is404 = !type || !arr.includes(type);
-    return { is404, type };
-  }
+  const type = window.location.pathname.split("/").filter(Boolean)[1];
+  const is404 = !type || !arr.includes(type);
+  return { is404, type };
+}
